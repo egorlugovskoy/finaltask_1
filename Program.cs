c8ba3f2533b22224800fb769f825f67622193e91
egorlugovskoy <112970451+egorlugovskoy@users.noreply.github.com>
2022-11-08 20:25:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+Clear();
+
+string[] full = new string[4];
+WriteLine("Введите строки");
+string s = FillArray(full);
+string[] cut = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+AddArray(full);
+AddArray(cut);
+WriteLine();
+Write("[" + string.Join(", ", full) + "] -> ");
+Write("[" + string.Join(", ", cut) + "]");
+
+string FillArray(string[] data)
+{
+    string row = string.Empty;
+    for (int i = 0; i < data.Length; i++)
+    {
+        Write($"Строка {i + 1}: ");
+        data[i] = Console.ReadLine() + "";
+        TestRow(data, i);
+        if (data[i].Length <= 3)
+        {
+            row += " " + data[i];
+        }
+    }
+    return row;
+}
+void AddArray(string[] data)
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] = "\"" + data[i] + "\"";
+    }
+}
+void TestRow(string[] data, int i)
+{
+    while (data[i] == "")
+    {
+        WriteLine($"Строка пустая, введите строку {i + 1} заново");
+        Write($"Строка {i + 1}: ");
+        data[i] = Console.ReadLine() + "";
+    }
+}
